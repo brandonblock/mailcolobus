7dc61c7e5f26f53dd43420f76d9365b9f67b743c
Brandon Block <brandon.block@pm.me>
2023-03-29 21:58:19 -0500

    implement health check (#2)
    * add health check
    
    * first pass at health check and integration test
    
    * package structure and tests
    
    * use dynamic port

---
+++ tests/health_check.rs
@@ -0,0 +1,33 @@
+//! tests/health_check.rs
+use std::net::TcpListener;
+
+#[actix_web::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app().await;
+
+    // bring in reqwester
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind ranom port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+
+    // using non-binding let on the future to allow the test to exit
+    let _ = actix_web::rt::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
