409e1b0398d652706044a1f794639266f684d395
Brandon Block <brandon.block@pm.me>
2023-03-30 00:28:48 -0500

    initial persistence support

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
 use actix_web::dev::Server;
-use actix_web::{get, App, HttpResponse, HttpServer};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 use std::net::TcpListener;
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 #[get("/health_check")]
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[post("/subscriptions")]
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check))
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
         .listen(listener)?
         .run();
     Ok(server)
