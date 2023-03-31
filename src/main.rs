edaeef0073cd01f55960f92328d5a56788dc8b77
Brandon Block <brandon.block@pm.me>
2023-03-30 22:06:32 -0500

    test connecting to db

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use mailcolobus::configuration::get_configuration;
 use mailcolobus::startup::run;
 
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[actix_web::main]
@@ -8,10 +9,13 @@ async fn main() -> std::io::Result<()> {
     // Panic if we can't get our configs
     let configuration = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failec to connect to Postgres");
 
     // Start listener
     let listener = TcpListener::bind(address)?;
     let port = listener.local_addr().unwrap().port();
     println!("Starting on port {}", port);
-    run(listener)?.await
+    run(listener, connection)?.await
 }
