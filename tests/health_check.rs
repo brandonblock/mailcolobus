71c191b9946d33e76928c924af95a7413255b292
Brandon Block <brandon.block@pm.me>
2023-04-14 23:15:36 -0500

    initial email api config and test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -42,7 +42,11 @@ async fn spawn_app() -> TestApp {
         .email_client
         .sender()
         .expect("invalid sender email address");
-    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
 
     let server = run(listener, connection.clone(), email_client).expect("Failed to bind address");
 
